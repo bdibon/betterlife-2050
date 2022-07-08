e674331755fead49368951bd5ee63719833ca8c3
Boris Dibon <boris.dibon@gmail.com>
2022-07-08 13:12:45 +0200

    Add utility methods on CustomUser

--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,17 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    pass
+    def is_member_of_an_organization(self):
+        return self.organizations.count()
+
+    def belongs_to_organization(self, organization):
+        return organization in self.organizations.all() or self in organization.admins.all()
+
+    def is_project_member(self, project):
+        return self in project.members.all() or self == project.owner
+
+    def is_project_owner(self, project):
+        return self == project.owner
+
+    def is_organization_admin(self, organization):
+        return self in organization.admins.all()
