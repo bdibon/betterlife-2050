0118b61fdabd9dfff7d76350e9c4c97fd8cd6bba
Boris Dibon <boris.dibon@gmail.com>
2022-07-08 14:22:53 +0200

    Update accounts view

--- accounts/views.py
+++ accounts/views.py
@@ -4,29 +4,39 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 
 
 from projects.models import Organization, Project
-from projects.serializers import OrganizationSerializer, ProjectSerializer
+from projects.serializers import OrganizationSerializer, ProjectSerializerReadOnly
 
 from .serializers import CustomUserSerializer
 
 
-
 class CustomUserViewSet(ModelViewSet, RetrieveModelMixin, ListModelMixin):
-    queryset = get_user_model().objects.all()
+    queryset = get_user_model().objects.all().order_by('date_joined')
     serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_superuser:
+            return self.queryset
+        if user.is_member_of_an_organization():
+            return self.queryset.filter(organizations__in=user.organizations.all())
+        return self.queryset.filter(pk=user.pk)
 
     @action(methods=['get'], detail=True, url_path="projects")
     def get_projects(self, request, pk=None):
         projects = Project.objects.filter(members__id__in=[pk])
-        serializer = ProjectSerializer(projects, many=True)
+        serializer = ProjectSerializerReadOnly(projects, many=True)
         return Response(serializer.data)
 
     @action(methods=['get'], detail=True, url_path="owned-projects")
     def get_owned_projects(self, request, pk=None):
         projects = Project.objects.filter(owner=pk)
-        serializer = ProjectSerializer(projects, many=True)
+        serializer = ProjectSerializerReadOnly(projects, many=True)
         return Response(serializer.data)
 
     @action(methods=['get'], detail=True, url_path="organizations")
